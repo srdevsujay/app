fd970d258c7d55fb10a9ad9d8775d97c6a5d1304
Bryan Foronda <brianforonda94@gmail.com>
2023-09-22 14:37:34 -0500

    integraciones

--- src/components/alerts/IntegrationAlert.tsx
+++ src/components/alerts/IntegrationAlert.tsx
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 export const IntegrationAlert = (title: string, navigate: any) => {
   toast.info(`${title} 'Clic para ir a integraciones'`, {
     position: toast.POSITION.TOP_RIGHT,
-    autoClose: 5000,
+    autoClose: 15000,
     onClick: () => {
       navigate("/configuracion");
     },
